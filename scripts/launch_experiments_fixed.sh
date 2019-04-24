71811cc394917a679edf09a1fa993e1d1be375b5
dbeley <6568955+dbeley@users.noreply.github.com>
2019-04-24 17:22:50 +0200

    scripts : meilleur affichage

--- scripts/launch_experiments_fixed.sh
+++ scripts/launch_experiments_fixed.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 # Expériences avec échantillonnage fixe
 # À lancer depuis la racine du projet
-printf "database fixed\n"
+printf "launch_experiments_fixed.sh - Expériences avec échantillonnage fixe avec tous les encodeurs"
+printf "1/10 - database fixed\n"
 python model.py -m svm -a -d datapapers -n database -f
-printf "datamining fixed\n"
+printf "2/10 - datamining fixed\n"
 python model.py -m svm -a -d datapapers -n datamining -f
-printf "medical fixed\n"
+printf "3/10 - medical fixed\n"
 python model.py -m svm -a -d datapapers -n medical -f
-printf "theory fixed\n"
+printf "4/10 - theory fixed\n"
 python model.py -m svm -a -d datapapers -n theory -f
-printf "visu fixed\n"
+printf "5/10 - visu fixed\n"
 python model.py -m svm -a -d datapapers -n visu -f 
-printf "database fixed wihout preprocessing\n"
+printf "6/10 - database fixed wihout preprocessing\n"
 python model.py -m svm -a -d datapapers -n database -f -p
-printf "datamining fixed without preprocessing\n"
+printf "7/10 - datamining fixed without preprocessing\n"
 python model.py -m svm -a -d datapapers -n datamining -f -p
-printf "medical fixed without preprocessing\n"
+printf "8/10 - medical fixed without preprocessing\n"
 python model.py -m svm -a -d datapapers -n medical -f -p
-printf "theory fixed without preprocessing\n"
+printf "9/10 - theory fixed without preprocessing\n"
 python model.py -m svm -a -d datapapers -n theory -f -p
-printf "visu fixed without preprocessing\n"
+printf "10/10 - visu fixed without preprocessing\n"
 python model.py -m svm -a -d datapapers -n visu -f -p
--- scripts/launch_experiments_novelty.sh
+++ scripts/launch_experiments_novelty.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 # Expériences sur toutes les modalités de datapapers
 # À lancer depuis la racine du projet
-printf "database\n"
+printf "launch_experiments_novelty.sh - Expériences sur toutes les modalités de datapapers avec sent2vec"
+printf "1/10 - database\n"
 python model.py -m svm -e sent2vec -d datapapers -n database
-printf "datamining\n"
+printf "2/10 - datamining\n"
 python model.py -m svm -e sent2vec -d datapapers -n datamining
-printf "medical\n"
+printf "3/10 - medical\n"
 python model.py -m svm -e sent2vec -d datapapers -n medical
-printf "theory\n"
+printf "4/10 - theory\n"
 python model.py -m svm -e sent2vec -d datapapers -n theory
-printf "visu\n"
+printf "5/10 - visu\n"
 python model.py -m svm -e sent2vec -d datapapers -n visu
-printf "database fixed\n"
+printf "6/10 - database fixed\n"
 python model.py -m svm -e sent2vec -d datapapers -n database -f
-printf "datamining fixed\n"
+printf "7/10 - datamining fixed\n"
 python model.py -m svm -e sent2vec -d datapapers -n datamining -f
-printf "medical fixed\n"
+printf "8/10 - medical fixed\n"
 python model.py -m svm -e sent2vec -d datapapers -n medical -f
-printf "theory fixed\n"
+printf "9/10 - theory fixed\n"
 python model.py -m svm -e sent2vec -d datapapers -n theory -f
-printf "visu fixed\n"
+printf "10/10 - visu fixed\n"
 python model.py -m svm -e sent2vec -d datapapers -n visu -f
