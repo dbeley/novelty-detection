e42dc7d5867ee7149dc2bfc9b3a41f0440fdadbc
dbeley <6568955+dbeley@users.noreply.github.com>
2019-04-24 17:25:25 +0200

    scripts : ajout unfixed

--- scripts/launch_experiments_unfixed.sh
+++ scripts/launch_experiments_unfixed.sh
@@ -3,22 +3,22 @@
 # À lancer depuis la racine du projet
 printf "launch_experiments_fixed.sh - Expériences avec échantillonnage fixe avec tous les encodeurs"
 printf "1/10 - database fixed\n"
-python model.py -m svm -a -d datapapers -n database -f
+python model.py -m svm -a -d datapapers -n database
 printf "2/10 - datamining fixed\n"
-python model.py -m svm -a -d datapapers -n datamining -f
+python model.py -m svm -a -d datapapers -n datamining
 printf "3/10 - medical fixed\n"
-python model.py -m svm -a -d datapapers -n medical -f
+python model.py -m svm -a -d datapapers -n medical
 printf "4/10 - theory fixed\n"
-python model.py -m svm -a -d datapapers -n theory -f
+python model.py -m svm -a -d datapapers -n theory
 printf "5/10 - visu fixed\n"
-python model.py -m svm -a -d datapapers -n visu -f
+python model.py -m svm -a -d datapapers -n visu
 printf "6/10 - database fixed wihout preprocessing\n"
-python model.py -m svm -a -d datapapers -n database -f -p
+python model.py -m svm -a -d datapapers -n database -p
 printf "7/10 - datamining fixed without preprocessing\n"
-python model.py -m svm -a -d datapapers -n datamining -f -p
+python model.py -m svm -a -d datapapers -n datamining -p
 printf "8/10 - medical fixed without preprocessing\n"
-python model.py -m svm -a -d datapapers -n medical -f -p
+python model.py -m svm -a -d datapapers -n medical -p
 printf "9/10 - theory fixed without preprocessing\n"
-python model.py -m svm -a -d datapapers -n theory -f -p
+python model.py -m svm -a -d datapapers -n theory -p
 printf "10/10 - visu fixed without preprocessing\n"
-python model.py -m svm -a -d datapapers -n visu -f -p
+python model.py -m svm -a -d datapapers -n visu -p
