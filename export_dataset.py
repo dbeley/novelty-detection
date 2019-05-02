82be83b11ac9c53cc8ca77dc042915aae7986257
dbeley <6568955+dbeley@users.noreply.github.com>
2019-05-02 13:31:49 +0200

    ajout pipenv

---
+++ export_dataset.py
@@ -0,0 +1,125 @@
+"""
+Script d'export des jeux de données
+"""
+
+import pandas as pd
+import random
+import argparse
+import logging
+import time
+import os
+
+logger = logging.getLogger()
+
+temps_debut = time.time()
+
+# Variables globales
+PATH_DATAPAPERS = os.path.expanduser("~/Documents/Données/datapapers.csv")
+PATH_DATAPAPERS_CLEAN = os.path.expanduser("~/Documents/Stage/sentences_embeddings/Exports/datapapers_clean.csv")
+
+# historic, context, novelty
+SAMPLES_LIST = [[2000, 300, 5],
+                [2000, 300, 10],
+                [2000, 300, 50]
+                # [2000, 300, 50],
+                # [2000, 300, 150],
+                # [2000, 300, 280],
+                # [5000, 300, 50],
+                # [5000, 300, 150],
+                # [5000, 300, 280],
+                # [2000, 1000, 50],
+                # [2000, 1000, 100],
+                # [2000, 1000, 250],
+                # [2000, 1000, 500],
+                ]
+# seed à utiliser pour l'échantillonnage
+SEEDS = [1, 4, 25]
+
+
+def split_data(data, size_historic, size_context, size_novelty, theme, fix_seed=9583):
+    """ Fonction qui genere le contexte et l historique """
+    random.seed(fix_seed)
+
+    novelty = data[data.theme == str(theme)]
+    no_novelty = data[data.theme != str(theme)]
+    idx_novelty = list(novelty.index)
+    idx_no_novelty = list(no_novelty.index)
+
+    idx_all = random.sample(idx_no_novelty, size_historic + size_context - size_novelty)
+    idx_historic = idx_all[0:size_historic]
+    idx_context = idx_all[size_historic:] + random.sample(idx_novelty, size_novelty)
+    data_historic = data.iloc[idx_historic]
+    data_context = data.iloc[idx_context]
+
+    return data_historic, data_context
+
+
+def main():
+    args = parse_args()
+
+    # Parsage des arguments
+    dataset = args.dataset
+    without_preprocessing = args.without_preprocessing
+    theme = args.novelty
+
+    # Chargement du jeu de données
+    if dataset == 'datapapers':
+        if without_preprocessing:
+            logger.info("Utilisation du jeu de données datapapers.csv")
+            data_filename = "datapapers.csv"
+            try:
+                data = pd.read_csv(PATH_DATAPAPERS, sep="\t", encoding="utf-8")
+                data = data.drop(['id', 'conf', 'title', 'author', 'year', 'eq', 'conf_short'], axis=1)
+            except Exception as e:
+                logger.error(str(e))
+                logger.error(f"Fichier {data_filename} non trouvé.")
+                exit()
+        else:
+            logger.info("Utilisation du jeu de données datapapers_clean.csv")
+            data_filename = "datapapers_clean.csv"
+            try:
+                data = pd.read_csv(PATH_DATAPAPERS_CLEAN)
+                data.columns = ['id', 'abstract', 'theme']
+                data = data.drop(['id'], axis=1)
+            except Exception as e:
+                logger.error(str(e))
+                logger.error(f"Fichier {data_filename} non trouvé. Lancez le script prepare.py.")
+                exit()
+    elif dataset is None:
+        logger.error(f"Entrez un jeu de données avec l'argument -d/--dataset")
+        exit()
+    else:
+        logger.error(f"Jeu de données {dataset} non supporté.")
+        exit()
+
+    # Boucle sur les seeds et les paramètres d'échantillons
+    for seed in SEEDS:
+        logger.info("Seed : {seed}")
+        for exp in SAMPLES_LIST:
+            # Récupération des paramètres d'échantillons
+            size_historic = exp[0]
+            size_context = exp[1]
+            size_novelty = exp[2]
+            logger.info(f"historique : {size_historic}, contexte : {size_context}, nouveauté : {size_novelty}")
+
+            data_historic, data_context = split_data(data, size_historic=size_historic, size_context=size_context, size_novelty=size_novelty, theme=theme, fix_seed=seed)
+            data_historic.to_csv(f"Exports/historic_s{seed}_{size_historic}_{size_context}_{size_novelty}_{theme}.csv", sep='\t')
+            data_context.to_csv(f"Exports/context_s{seed}_{size_historic}_{size_context}_{size_novelty}_{theme}.csv", sep='\t')
+
+    logger.info("Temps d'exécution : %.2f secondes" % (time.time() - temps_debut))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Script d'export des jeux de données découpés en historique/contexte")
+    parser.add_argument('--debug', help="Display debugging information", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
+    parser.add_argument('-p', '--without_preprocessing', help="Utilise le jeu de données sélectionné, mais sans pré-traitement (phrases complètes)", dest='without_preprocessing', action='store_true')
+    parser.add_argument('-n', '--novelty', help="Nouveauté à découvrir (défaut = 'theory')", type=str, default='theory')
+    parser.set_defaults(without_preprocessing=False)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.loglevel)
+    return args
+
+
+if __name__ == '__main__':
+    main()
---
+++ prepare_nyt.py
@@ -0,0 +1,60 @@
+import pandas as pd
+# import matplotlib.pyplot as plt
+import os
+# %matplotlib inline
+
+# Jeu de données a utiliser
+DATASET = os.path.expanduser("~/Documents/Données/data_big_category_long.csv")
+
+df = pd.read_csv(DATASET, sep="\t", encoding = "ISO-8859-1", engine='python')
+
+df.dates = pd.to_datetime(df.dates)
+df['year'] = df.dates.dt.year
+
+print("Size : %s \n" % str(df.shape))
+
+# Pour ne pas travailler sur tout le NYT, on va se limiter dans un premier temps aux 10 premières années 
+# et a un certain nombre de catégories
+
+# Catégories à garder :
+# - Football
+# - Finances
+# - Music
+# - Theater
+# - Elections
+# - Housing
+# - Motion Pictures
+# - Art
+# - Terrorism
+# - United states international relations
+
+
+category_list = ['football', 'finances', 'music','theater', 'elections', 'housing', 'motion pictures', 'art', 'terrorism','united states international relations']
+
+
+small_df = df[(df['principal_classifier'].isin(category_list)) & (df['dates'] < '1998-01-01')]
+print(small_df.shape)
+temporal_df = small_df.groupby(['dates'])['texts'].count()
+temporal_df.plot(figsize=(20,5), title="Nombre d'articles par jour")
+def plot_categorie(data,categorie, granularity):
+    data.index = data.dates
+    category_selection = data[data['principal_classifier'] == categorie]
+    categorie_evolution = category_selection.groupby(['principal_classifier'])
+    toplot = categorie_evolution.resample(granularity).count()['texts']
+    toplot = toplot.reset_index()
+    toplot.index = toplot.dates
+
+    ax = toplot.plot(title=categorie)
+    plt.show()
+    
+    return toplot
+
+# for cat in category_list:
+#     signal = plot_categorie(small_df.copy(), cat, '1w')
+
+# Historique : on peut utiliser les 3 premières années comme historique (1987,1988,1989)
+# Détecter la nouveauté sur 1990-1997
+
+# Il faut tester les méthodes en enlevant chacune des catégorie de l'historique une a une.
+
+small_df.to_csv('experimentations.csv', index=None, sep='\t')
